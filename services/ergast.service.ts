8c67a8c090c365377f7e41724162faa5f69a211f
Pete West <10847430+peterwest-1@users.noreply.github.com>
2023-03-28 10:29:23 +0200

    Updated service to fetch champ for year

--- services/ergast.service.ts
+++ services/ergast.service.ts
@@ -8,8 +8,24 @@ export const getChamps = async () => {
 };
 
 export const getSeason = async (year: string) => {
-  const response = await fetch(`https://ergast.com/api/f1/${parseInt(year)}/results/1.json`);
+  const url = `https://ergast.com/api/f1/${parseInt(year)}/results/1.json`;
+  const response = await fetch(url);
   const json = await response.json();
   const list = json.MRData.RaceTable.Races as Race[];
   return list;
+};
+
+export const getChampForYear = async (year: string) => {
+  const offset = parseInt(year) - 1950;
+  const url = `https://ergast.com/api/f1/driverStandings/1.json?limit=1&offset=${offset}`;
+
+  //TODO: Find proper way to get champ for specific year
+  const response = await fetch(url);
+  const json = await response.json();
+  const list = json.MRData.StandingsTable.StandingsLists[0].DriverStandings[0].Driver;
+
+  return {
+    givenName: list.givenName,
+    familyName: list.familyName,
+  };
 };
